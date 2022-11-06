1133c157353b9b539332584fdcc25c497c1cf722
Haley B <haleybiggs114@gmail.com>
2022-11-05 21:09:46 -0400

    replace local host

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     function getBaseUrl(endpoint){
-        return 'https://capstoneoutfitters.azurewebsites.net'+endpoint;
+        return 'capstoneoutfitters.azurewebsites.net'+endpoint;
     }
     $('#login_form').submit(function(){
         $('#login_alert').hide();
