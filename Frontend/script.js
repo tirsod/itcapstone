fd87b79a4705e418149ca4c55881d3f120aa9db9
Haley B <haleybiggs114@gmail.com>
2022-11-20 14:11:05 -0500

    forcing back button page reload

--- Frontend/script.js
+++ Frontend/script.js
@@ -56,3 +56,13 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+window.addEventListener( "pageshow", function ( event ) {
+    var historyTraversal = event.persisted || 
+                           ( typeof window.performance != "undefined" && 
+                                window.performance.navigation.type === 2 );
+    if ( historyTraversal ) {
+      // Handle page restore.
+      window.location.reload();
+    }
+  });
